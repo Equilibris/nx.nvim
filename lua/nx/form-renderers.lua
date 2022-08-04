a33730ba07190cd56daafbfd3f739c4e307f20a5
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-08-04 15:28:19 +0200

    feat: baseline for form rendering

---
+++ lua/nx/form-renderers.lua
@@ -0,0 +1,10 @@
+-- Form renderers take a JSON schema as an input and must emit a table as an output
+--
+-- The telescope form renderer is composed of two components
+--
+-- 1. A general renderer;
+--		a component that renders all the options viewed from coposition
+-- 2. A component renderer;
+--		a component that renders induvitual components standalone
+
+local telescope_form_renderer = function(form) end
