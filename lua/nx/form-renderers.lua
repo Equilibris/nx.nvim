4a37572d187e3ea8649b85ab71b30c6ca9ccca4d
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-08-05 13:22:40 +0200

    feat: telescope form renderer

--- lua/nx/form-renderers.lua
+++ lua/nx/form-renderers.lua
@@ -7,4 +7,275 @@
 -- 2. A component renderer;
 --		a component that renders induvitual components standalone
 
-local telescope_form_renderer = function(form) end
+_G.dump = function(o, level)
+	level = level or 1
+
+	local indent = string.rep('  ', level)
+
+	if type(o) == 'table' then
+		local s = '{\n'
+		for k, v in pairs(o) do
+			if type(k) ~= 'number' then
+				k = '"' .. k .. '"'
+			end
+			s = s .. indent .. '[' .. k .. '] = ' .. dump(v, level + 1) .. ',\n'
+		end
+		return s .. indent .. '}'
+	else
+		return tostring(o)
+	end
+end
+
+_G.pd = function(o)
+	print(dump(o))
+end
+
+local pickers = require 'telescope.pickers'
+local finders = require 'telescope.finders'
+local conf = require('telescope.config').values
+
+local entry_display = require 'telescope.pickers.entry_display'
+
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
+
+local _M = {}
+
+local make_entry = function(opts, internal_state)
+	return function(entry)
+		if entry.is_done then
+			return { value = entry, display = 'Submit', ordinal = 'Submit' }
+		else
+			local x = entry.key
+				.. (entry.required and '*' or '')
+				.. ' :: '
+				.. entry.type
+
+			local mod = internal_state[entry.key] ~= nil
+					and ' ( ' .. tostring(internal_state[entry.key]) .. ' )'
+				or ''
+			return { value = entry, display = x .. mod, ordinal = x }
+		end
+	end
+	-- local displayer = entry_display.create {
+	-- 	separator = ' ',
+	-- 	items = {
+	-- 		{ width = 8 },
+	-- 		{ remaining = true },
+	-- 	},
+	-- }
+
+	-- return function(entry)
+	-- 	if entry.is_done then
+	-- 		return displayer {
+	-- 			{ '', 'TelescopeResultsIdentifier' },
+	-- 			'submit',
+	-- 		}
+	-- 	else
+	-- 		return displayer {
+	-- 			{ entry.type, 'TelescopeResultsIdentifier' },
+	-- 			entry.key,
+	-- 		}
+	-- 	end
+	-- end
+end
+
+_M.telescope_form_renderer = function(opts)
+	local renderer
+
+	renderer = function(form, title, callback, state)
+		title = title or form['$id']
+
+		local type = form.type
+
+		if type == 'object' then
+			local keys = {}
+
+			local supplied_req = 0
+			local required = form.required or {}
+			local set = {}
+
+			for _, value in ipairs(required) do
+				set[value] = true
+			end
+
+			for key, value in pairs(form.properties) do
+				table.insert(keys, {
+					type = value.type,
+					key = key,
+					required = set[key] or false,
+				})
+
+				if state[key] == nil and value.default ~= nil then
+					state[key] = value.default
+				end
+				if set[key] and state[key] then
+					supplied_req = supplied_req + 1
+				end
+			end
+
+			if supplied_req == #required then
+				table.insert(keys, { is_done = true })
+			end
+
+			local object_picker = pickers.new(opts, {
+				prompt_title = title,
+
+				finder = finders.new_table {
+					results = keys,
+					entry_maker = make_entry(opts, state),
+				},
+				sorter = conf.generic_sorter(opts),
+				attach_mappings = function(prompt_bufnr, map)
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local selection =
+							action_state.get_selected_entry().value
+
+						if selection.is_done then
+							callback(state)
+						else
+							renderer(
+								form.properties[selection.key],
+								title .. '.' .. selection.key,
+								function(value)
+									state[selection.key] = value
+
+									renderer(form, title, callback, state)
+								end,
+								state[selection.key]
+							)
+						end
+					end)
+
+					return true
+				end,
+			})
+
+			object_picker:find()
+		elseif type == 'string' then
+			if
+				form['$default'] ~= nil
+				and _G.nx.workspace
+				and form['$default']['$source'] == 'projectName'
+			then
+				local projects = {}
+				for key, _ in pairs(_G.nx.projects) do
+					table.insert(projects, key)
+				end
+
+				local project_picker = pickers.new(opts, {
+					prompt_title = title,
+					finder = finders.new_table {
+						results = projects,
+					},
+					sorter = conf.generic_sorter(opts),
+					attach_mappings = function(prompt_bufnr, map)
+						actions.select_default:replace(function()
+							actions.close(prompt_bufnr)
+							local selection =
+								action_state.get_selected_entry().value
+							callback(selection)
+						end)
+
+						return true
+					end,
+				})
+				project_picker:find()
+			else
+				local value = vim.fn.input(title .. ': ', state or '')
+				callback(value)
+			end
+		elseif type == 'number' then
+			local value = vim.fn.input(title .. ' (numeric): ', state or '')
+
+			callback(tonumber(value))
+		elseif type == 'boolean' then
+			local bool_picker = pickers.new(opts, {
+				prompt_title = title,
+				finder = finders.new_table {
+					results = { true, false },
+					entry_maker = function(entry)
+						if entry then
+							return {
+								value = entry,
+								display = 'true',
+								ordinal = 'trueyes1',
+							}
+						else
+							return {
+								value = entry,
+								display = 'false',
+								ordinal = 'falseno0',
+							}
+						end
+					end,
+				},
+				sorter = conf.generic_sorter(opts),
+				attach_mappings = function(prompt_bufnr, map)
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local selection =
+							action_state.get_selected_entry().value
+						callback(selection)
+					end)
+
+					return true
+				end,
+			})
+			bool_picker:find()
+		end
+	end
+
+	return renderer
+end
+
+_G.test = function()
+	_M.telescope_form_renderer()(
+		vim.json.decode [[
+{
+	"$schema": "http://json-schema.org/schema",
+	"cli": "nx",
+	"$id": "styled-component",
+	"type": "object",
+	"properties": {
+		"project": {
+			"type": "string",
+			"description": "The name of the project.",
+			"alias": "p",
+			"$default": {
+				"$source": "projectName"
+			},
+			"x-prompt": "What is the name of the project for this component?"
+		},
+		"name": {
+			"type": "string",
+			"description": "The name of the component.",
+			"$default": {
+				"$source": "argv",
+				"index": 0
+			},
+			"x-prompt": "What name would you like to use for the component?",
+			"pattern": "^[A-Z][a-zA-Z].*$"
+		},
+		"component": {
+			"type": "string",
+			"description": "The name of the parent component.",
+			"x-prompt": "What name would you like to use as the parent component?"
+		},
+		"mui": {
+			"type": "boolean",
+			"description": "Use MUI.",
+			"default": false
+		}
+	},
+	"required": ["name", "project", "component"]
+}
+]],
+		nil,
+		nil,
+		{}
+	)
+end
+
+return _M
